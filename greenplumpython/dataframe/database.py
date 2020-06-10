53e5f8a2c26df2e09a689276d7364992e1766723
wengyanqing <wengyanqing@gmail.com>
2020-06-10 08:51:50 +0800

    get connection id from database

--- greenplumpython/dataframe/database.py
+++ greenplumpython/dataframe/database.py
@@ -1,6 +1,12 @@
 from pandas.io.sql import SQLDatabase, SQLTable, _convert_params, _wrap_result
 from .dataframe_wrapper import DataFrameWrapper
+from greenplumpython.connection.gp import GPConnection
+
 class GPDatabase(SQLDatabase):
+    def __init__(self):
+        self.max_conn_id = 0
+        self.conns = {}
+
     def read_table(self,
         table_name,
         index_col=None,
@@ -38,3 +44,12 @@ class GPDatabase(SQLDatabase):
             parse_dates=parse_dates,
         )
         return frame
+    def connect(self,
+            dbip,
+            dbname,
+            username,
+            password=None):
+        conn = GPConnection(dbip, dbname, username, password).get_connection()
+        self.max_conn_id += 1
+        self.conns[self.max_conn_id] = conn
+        return self.max_conn_id
