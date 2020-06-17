cfd01ef9b67514c6f3917f0115c4007a8d59ec88
Violet Cheng <vcheng@pivotal.io>
2020-06-17 16:26:29 +0800

    fix test and add db connection port param

--- greenplumpython/connection/gp.py
+++ greenplumpython/connection/gp.py
@@ -2,16 +2,16 @@ from pg import connect
 import sqlalchemy
 class GPConnection(object):
 
-    def __init__(self, host: str, database: str, user: str, password: str):
+    def __init__(self, host: str, port: int, database: str, user: str, password: str):
         self.connection_pool = None
         self.host = host
+        self.port = port
         self.database = database
         self.user = user
         self.password = password
 
     def get_connection(self):
-        #TODO port
-        url = "postgresql+pygresql://%s:%s@%s:6000/%s" % (self.user, self.password, self.host, self.database)
+        url = "postgresql+pygresql://%s:%s@%s:%d/%s" % (self.user, self.password, self.host, self.port, self.database)
         engine = sqlalchemy.create_engine(url)
         #connection = connect(url)
         return engine.connect()
--- greenplumpython/tests/dataframe/test_getdataframewrapper.py
+++ greenplumpython/tests/dataframe/test_getdataframewrapper.py
@@ -1,12 +1,18 @@
 from greenplumpython.connection.gp import GPConnection
 from greenplumpython.dataframe import sql
 from greenplumpython.dataframe.database import GPDatabase
+from greenplumpython.dataframe.sql import _sqlalchemy_con
 import os
 import pytest
 
 host = os.getenv('GPHOST')
 if host is None or host is '':
     host = "localhost"
+port = os.getenv('GPPORT')
+if port is None or port is '':
+    port = 5432
+else:
+    port = int(port)
 db = os.getenv('GPDATABASE')
 if db is None or db is '':
     db = "gppython"
@@ -18,7 +24,7 @@ password = os.getenv('GPPASSWORD')
 @pytest.fixture(scope='session', autouse=True)
 def db_conn():
     # Will be executed before the first test
-    sql_conn = GPConnection(host, db, user, password).get_connection()
+    sql_conn = GPConnection(host, port, db, user, password).get_connection()
     yield sql_conn
     # Will be executed after the last test
     sql_conn.close()
@@ -41,28 +47,28 @@ def test_has_table(db_conn):
     result = sql.has_table("employee1", db_conn)
     assert result == False
 
-def test_database_conn_success():
-    dbinstance = GPDatabase()
-    connid = dbinstance.connect(host, db, user, password)
+def test_database_conn_success(db_conn):
+    dbinstance = GPDatabase(None)
+    connid = dbinstance.connect(host, port, db, user, password)
     assert connid == 1
 
 def test_database_conn_fail():
     with pytest.raises(Exception) as e:
-        dbinstance = GPDatabase()
-        assert dbinstance.connect(host, 'no_exist_db', user, password)
+        dbinstance = GPDatabase(None)
+        assert dbinstance.connect(host, port, 'no_exist_db', user, password)
 
 def test_database_conn_list():
-    dbinstance = GPDatabase()
-    connid = dbinstance.connect(host, db, user, password)
+    dbinstance = GPDatabase(None)
+    connid = dbinstance.connect(host, port, db, user, password)
     assert connid == 1
-    connid2 = dbinstance.connect(host, db, user, password)
+    connid2 = dbinstance.connect(host,port, db, user, password)
     assert connid2 == 2
     assert dbinstance.list() == [1, 2]
 
 def test_database_conn_close():
-    dbinstance = GPDatabase()
-    connid = dbinstance.connect(host, db, user, password)
-    connid2 = dbinstance.connect(host, db, user, password)
+    dbinstance = GPDatabase(None)
+    connid = dbinstance.connect(host,port, db, user, password)
+    connid2 = dbinstance.connect(host,port, db, user, password)
     assert dbinstance.list() == [1, 2]
     dbinstance.close(connid)
     assert dbinstance.list() == [2]
