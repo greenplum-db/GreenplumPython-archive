d732ac1992d26191efbaad1ba741b51856aab4cd
Violet Cheng <vcheng@pivotal.io>
2020-06-18 10:52:35 +0800

    change metadata to gptable_schema (#10)

--- greenplumpython/dataframe/gptable_schema.py
+++ greenplumpython/dataframe/gptable_schema.py
@@ -1,5 +1,5 @@
 import re
-class Metadata():
+class GPTableSchema():
     def __init__(self, name, signature = dict(), distribute_on = ''):
         if self.check_output_name(name):
             self.name = name
