a88a5c09615d656722167ec38f118d5c951c0b3a
Haozhou Wang <hawang@pivotal.io>
2020-07-22 09:57:47 +0000

    Update setup version and script file
    1. Add versioneer package to get version from git tag
    2. Update script file to collect all required files

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import setuptools
+import versioneer 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="greenplum-python", # Replace with your own username
-    version="0.0.1",
+    version=versioneer.get_version(),
     author="Greenplum Python",
     author_email="greenplum-python@vmware.com",
     description="Python interface for Greenplum",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/greenplum-db/GreenplumPython",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=["greenplumpython", "greenplumpython.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
