1f47dacdc66778ef6acfea8481a19382002ae241
Haozhou Wang <hawang@pivotal.io>
2020-07-23 17:38:25 +0800

    Update setup version and script file
    1. Add versioneer package to get version from git tag
    2. Update script file to collect all required files

---
+++ greenplumpython/__init__.py
@@ -0,0 +1,13 @@
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+from greenplumpython.utils.apply_utils import *
+from greenplumpython.core.dataframe_wrapper import DataFrameWrapper
+from greenplumpython.core.gp_connection import GPConnection
+from greenplumpython.core.gptable_metadata import GPTableMetadata
+from greenplumpython.core.gptapply import gptApply
+from greenplumpython.core.gpapply import gpApply
+from greenplumpython.core import sql
+
