e0ef1ca56a872ac9b9f25bb62f5dc560cd2302e3
Violet Cheng <vcheng@pivotal.io>
2020-07-31 15:19:31 +0800

    add performance test

---
+++ greenplumpython/tests/perf_test/perf_prepare.sql
@@ -0,0 +1,2 @@
+CREATE TABLE basic_numeric (a numeric, b numeric);
+INSERT INTO basic_numeric VALUES(1.13, 3.12), (2.21,69.12);
---
+++ greenplumpython/tests/perf_test/test_performance.py
@@ -0,0 +1,67 @@
+from greenplumpython.core.gpapply import gpApply
+from greenplumpython.tests.testdb import host, port, db, user, password
+from greenplumpython.core.gp_connection import GPConnection
+from greenplumpython.core.gpdatabase import GPDatabase
+from greenplumpython.core import sql
+from greenplumpython.core.gptable_metadata import GPTableMetadata
+import time
+from datetime import timedelta
+
+def py_func_1(a, b):
+    x = dict()
+    result = 0
+    for j in range(1, 100):
+        result = a
+        for i in range(1, 1000000):
+            result = result + b
+        
+    x['a'] = result
+    return x
+
+def py_func_2(a, b):
+    x = dict()
+    result = 0
+    for i in range(1, 1000000):
+        result = 0
+        result = a/b
+    x['a'] = result
+    return x
+
+if __name__ == "__main__":
+    db_conn = GPConnection().connect(host, port, db, user, password)
+    data = GPDatabase(db_conn)
+    table = data.get_table("basic", "public")
+    output_col = [{"a":"int4"}]
+    output = GPTableMetadata("basic_output", output_col, 'randomly')
+    start_time = time.monotonic()
+
+    gpApply(table, py_func_1, data, output, True, 'plc_python3_shared', 'plcontainer')
+    end_time = time.monotonic()
+    print("plus container", timedelta(seconds=end_time - start_time))
+    res = data.execute_query("select * from basic_output")
+    print(res)
+    start_time2 = time.monotonic()
+
+    gpApply(table, py_func_1, data, output)
+    end_time2 = time.monotonic()
+    res = data.execute_query("select * from basic_output")
+    print("plus pythonu",timedelta(seconds=end_time2 - start_time2))
+    print(res)
+
+    table_n = data.get_table("basic_numeric", "public")
+    output_col = [{"a":"numeric"}]
+    output = GPTableMetadata("basic_numeric_output", output_col, 'randomly')
+
+    gpApply(table_n, py_func_2, data, output, True, 'plc_python3_shared', 'plcontainer')
+    end_time = time.monotonic()
+    print("divide container", timedelta(seconds=end_time - start_time))
+    res = data.execute_query("select * from basic_numeric_output")
+    print(res)
+    start_time2 = time.monotonic()
+
+    gpApply(table_n, py_func_2, data, output)
+    end_time2 = time.monotonic()
+    res = data.execute_query("select * from basic_numeric_output")
+    print("divide pythonu",timedelta(seconds=end_time2 - start_time2))
+    print(res)
+    db_conn.close()
