63a05daa65f904b6090d9350e6e8b4685e99f6ec
Violet Cheng <vcheng@pivotal.io>
2020-08-27 17:48:33 +0800

    add release pipeline

---
+++ concourse/scripts/greenplumpython_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+#
+# Copyright (c) 2017-Present Pivotal Software, Inc
+#
+#------------------------------------------------------------------------------
+
+set -exo pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOP_DIR=${CWDIR}/../../../
+
+release_greenplumpython() {
+  pushd GreenplumPython_src
+  python3 -m pip install --user --upgrade setuptools wheel
+  python3 setup.py sdist bdist_wheel
+  popd
+  mkdir -p greenplumPython-release
+  cp ${TOP_DIR}/GreenplumPython_src/dist/greenplum-python-*.tar.gz greenplumPython-release/
+}
+
+release_greenplumpython
