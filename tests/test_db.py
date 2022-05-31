bf603bba858c3f338bd92304ec95faf65dd945eb
Xuebin Su (苏学斌) <12034000+xuebinsu@users.noreply.github.com>
2022-05-31 18:17:19 +0800

    Support function calls on constants (#43)
    Being able to call functions is essential for data analytics. This
    patch adds support for calling functions in Greenplum. Given
    that Column is not completed when the work starts, this patch
    only implements function calls on constants but not on
    tables. Changes are required to support function calls on tables.

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,5 +5,5 @@ def test_db():
     db = gp.database(host="localhost", dbname="gpadmin")
     result = db.execute("SELECT version()")
     for row in result:
-        assert "PostgreSQL" in row["version"]
+        assert "Greenplum" in row["version"]
     db.close()
