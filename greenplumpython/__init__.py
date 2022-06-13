2099256139219c79fb6e99bd7aaf1ec6c67e7702
Xuebin Su <sxuebin@vmware.com>
2022-06-12 23:06:17 -0400

    Sort imports

--- greenplumpython/__init__.py
+++ greenplumpython/__init__.py
@@ -1,3 +1,3 @@
 from .db import Database, database
-from .func import create_function, function, aggregate, create_aggregate
+from .func import aggregate, create_aggregate, create_function, function
 from .table import Table, table, values
