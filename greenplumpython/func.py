3729cdcf747c569ae8fc8f15abd1e35a54d0ed6f
Xuebin Su (苏学斌) <12034000+xuebinsu@users.noreply.github.com>
2022-06-15 22:43:50 +0800

    Support calling and creating AGGREGATEs (#51)
    For data scientists, aggregates are essential for retrieving summaries
    of the data. This patch adds new functions for calling existing
    aggregates in Greenplum, and create new aggregates from Python
    functions.
    
    Note that this patch only includes most fundamental support for
    aggregates. Many features, e.g. final functions, combine functions,
    and composite state type, are NOT supported yet.

--- greenplumpython/func.py
+++ greenplumpython/func.py
@@ -2,7 +2,8 @@ import functools
 import inspect
 import re
 import textwrap
-from typing import Callable, Iterable, Optional
+from typing import Callable, Iterable, Optional, Union
+from uuid import uuid4
 
 from .db import Database
 from .expr import Expr
@@ -15,6 +16,7 @@ class FunctionCall(Expr):
         self,
         func_name: str,
         args: Iterable[Expr] = [],
+        group_by: Optional[Iterable[Union[Expr, str]]] = None,
         as_name: Optional[str] = None,
         db: Optional[Database] = None,
     ) -> None:
@@ -28,6 +30,7 @@ class FunctionCall(Expr):
         super().__init__(as_name, table=table, db=db)
         self._func_name = func_name
         self._args = args
+        self._group_by = group_by
 
     def __str__(self) -> str:
         args_string = ",".join([str(arg) for arg in self._args]) if any(self._args) else ""
@@ -36,13 +39,32 @@ class FunctionCall(Expr):
     def to_table(self) -> Table:
         as_string = f"AS {self._as_name}" if self._as_name is not None else ""
         from_caluse = f"FROM {self.table.name}" if self.table is not None else ""
+        group_by_columns = (
+            ",".join([str(column) for column in self._group_by])
+            if self._group_by is not None
+            else ""
+        )
+        group_by_clause = f"GROUP BY {group_by_columns}" if self._group_by is not None else ""
         parents = [self.table] if self.table is not None else []
         orig_func_table = Table(
-            f"SELECT {str(self)} {as_string} {from_caluse}", db=self._db, parents=parents
+            " ".join(
+                [
+                    f"SELECT {str(self)} {as_string}",
+                    "," + group_by_columns if group_by_columns != "" else "",
+                    from_caluse,
+                    group_by_clause,
+                ]
+            ),
+            db=self._db,
+            parents=parents,
         )
         return Table(
             f"SELECT * FROM {orig_func_table.name}", parents=[orig_func_table], db=self._db
         )
+
+    @property
+    def qualified_func_name(self) -> str:
+        return self._func_name
 
 
 def function(name: str, db: Database) -> Callable[..., FunctionCall]:
@@ -52,6 +74,18 @@ def function(name: str, db: Database) -> Callable[..., FunctionCall]:
     return make_function_call
 
 
+def aggregate(name: str, db: Database) -> Callable[..., FunctionCall]:
+    def make_function_call(
+        *args: Expr,
+        group_by: Optional[Iterable[Union[Expr, str]]] = None,
+        as_name: Optional[str] = None,
+    ) -> FunctionCall:
+        return FunctionCall(name, args, group_by=group_by, as_name=as_name, db=db)
+
+    return make_function_call
+
+
+# FIXME: Add test cases for optional parameters
 def create_function(
     func: Callable,
     name: Optional[str] = None,
@@ -65,16 +99,18 @@ def create_function(
         *args: Expr, as_name: Optional[str] = None, db: Optional[Database] = None
     ) -> FunctionCall:
         or_replace = "OR REPLACE" if replace_if_exists else ""
-        schema_qualifier = "pg_temp." if temp else f"{schema}." if schema is not None else ""
+        schema_name = "pg_temp" if temp else (schema if schema is not None else "")
         func_name = func.__name__ if name is None else name
-        qualified_func_name = schema_qualifier + func_name
+        if len(func_name) > 64:  # i.e. NAMELEN in PostgreSQL
+            raise Exception("Function name should be no longer than 64 bytes.")
+        qualified_func_name = ".".join([schema_name, func_name])
         if not temp and name is None:
             raise NotImplementedError("Name is required for a non-temp function")
         func_sig = inspect.signature(func)
         func_args_string = ",".join(
             [
-                f"{param_name} {primitive_type_map[func_sig.parameters[param_name].annotation]}"
-                for param_name in func_sig.parameters
+                f"{param.name} {primitive_type_map[param.annotation]}"
+                for param in func_sig.parameters.values()
             ]
         )
         # FIXME: include things in func.__closure__
@@ -97,6 +133,53 @@ def create_function(
             ),
             has_results=False,
         )
-        return FunctionCall(qualified_func_name, args, as_name=as_name, db=db)
+        return FunctionCall(qualified_func_name, args=args, as_name=as_name, db=db)
+
+    return make_function_call
+
+
+# FIXME: Add test cases for optional parameters
+def create_aggregate(
+    trans_func: Callable,
+    name: Optional[str] = None,
+    schema: Optional[str] = None,
+    temp: bool = True,
+    replace_if_exists: bool = False,
+    language_handler: str = "plpython3u",
+) -> Callable:
+    @functools.wraps(trans_func)
+    def make_function_call(
+        *args: Expr,
+        group_by: Optional[Iterable[Union[Expr, str]]] = None,
+        as_name: Optional[str] = None,
+        db: Optional[Database] = None,
+    ) -> FunctionCall:
+        trans_func_call = create_function(
+            trans_func, "func_" + uuid4().hex, schema, temp, replace_if_exists, language_handler
+        )(*args, as_name=as_name, db=db)
+        or_replace = "OR REPLACE" if replace_if_exists else ""
+        schema_name = "pg_temp" if temp else schema if schema is not None else ""
+        agg_name = trans_func.__name__ if name is None else name
+        qualified_agg_name = ".".join([schema_name, agg_name])
+        if not temp and name is None:
+            raise NotImplementedError("Name is required for a non-temp function")
+        sig = inspect.signature(trans_func)
+        param_list = iter(sig.parameters.values())
+        state_param = next(param_list)
+        args_string = ",".join(
+            [f"{param.name} {primitive_type_map[param.annotation]}" for param in param_list]
+        )
+        trans_func_call.db.execute(
+            f"""
+            CREATE {or_replace} AGGREGATE {qualified_agg_name} ({args_string}) (
+                SFUNC = {trans_func_call.qualified_func_name},
+                STYPE = {primitive_type_map[state_param.annotation]}
+            )
+            """,
+            has_results=False,
+        )
+        return FunctionCall(
+            qualified_agg_name, args=args, group_by=group_by, as_name=as_name, db=db
+        )
 
     return make_function_call
