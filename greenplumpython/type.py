caea6b9d76aeb592f0147681e2e07b12458caec9
Xuebin Su <sxuebin@vmware.com>
2022-06-16 23:08:17 -0400

    Use duck typing

--- greenplumpython/type.py
+++ greenplumpython/type.py
@@ -12,11 +12,11 @@ primitive_type_map = {
 
 # FIXME: Annotate the argument type for this function
 def to_pg_type(annotation) -> str:
-    if not isinstance(annotation, typing.GenericMeta):
-        if annotation in primitive_type_map:
-            return primitive_type_map[annotation]
-        raise NotImplementedError()  # TODO: Support composite types
-    else:
+    if hasattr(annotation, "__origin__"):
         if annotation.__origin__ == typing.List:
             return f"{to_pg_type(annotation.__args__[0])}[]"
         raise NotImplementedError()
+    else:
+        if annotation in primitive_type_map:
+            return primitive_type_map[annotation]
+        raise NotImplementedError()  # TODO: Support composite types
