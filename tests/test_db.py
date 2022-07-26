138c16967d92c6db73b0a7873b0d355eded1ff3f
yihong0618 <zouzou0208@gmail.com>
2022-07-26 16:54:04 +0800

    fix: one test conn failed

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,9 +7,9 @@ from tests import db
 def test_db():
     db = gp.database(
         host="localhost",
-        dbname=environ.get("POSTGRES_DB", "gpadmin"),
-        user=environ.get("POSTGRES_USER"),
-        password=environ.get("POSTGRES_PASSWORD"),
+        dbname=environ.get("TESTDB", "gpadmin"),
+        user=environ.get("PGUSER"),
+        password=environ.get("PGPASSWORD"),
     )
     result = db.execute("SELECT version()")
     for row in result:
