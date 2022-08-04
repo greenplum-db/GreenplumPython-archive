4510367049d926985df0c769cea0e1eeb4a0cb7b
Xing Guo <higuoxing+github@gmail.com>
2022-08-04 15:47:15 +0800

    [CI] Teach concourse to fetch secrets from Vault. (#76)
    This patch helps teach concourse to fetch secrets from Vault.

--- concourse/fly.sh
+++ concourse/fly.sh
@@ -109,9 +109,6 @@ set -v
     sp \
     -p "${pipeline_name}" \
     -c "${yml_path}" \
-    -l "${workspace}/gp-continuous-integration/secrets/gpdb_common-ci-secrets.yml" \
-    -l "${workspace}/gp-continuous-integration/secrets/gp-extensions-common.yml" \
-    -l "${workspace}/gp-continuous-integration/secrets/gpdb_6X_STABLE-ci-secrets.prod.yml" \
     -v "${proj_name}-branch=${branch}"
 set +v
 
