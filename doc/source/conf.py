789397df2330476336dc6a6cd1818d065b4f5f02
yihong0618 <zouzou0208@gmail.com>
2022-08-16 17:53:15 +0800

    fix: better to show repo and doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,6 +48,13 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 
+html_context = {
+    "display_github": True,
+    "github_user": "greenplum-db",
+    "github_repo": "GreenplumPython",
+    "github_version": "master/doc/source/",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
