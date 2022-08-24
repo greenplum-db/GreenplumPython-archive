302655810e99ea461f92f903c5278b2064128eda
yihong0618 <zouzou0208@gmail.com>
2022-08-24 11:01:55 +0800

    feat: prepare for PyPI upload

--- setup.py
+++ setup.py
@@ -1,28 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-
 setuptools.setup(
-    name="greenplum-python",
-    version="1.0.0-beta1",
-    install_requires=requirements,
+    name="greenplum-python-fork",
+    version="1.0.0-beta4",
+    install_requires=["psycopg2==2.9.3"],
     author="Greenplum Python",
     author_email="greenplum-python@vmware.com",
-    description="Python interface for Greenplum",
-    long_description=long_description,
+    description="Python interface for Greenplum and Postgres",
+    long_description="Python interface for Greenplum and Postgres",
     long_description_content_type="text/markdown",
     url="https://github.com/greenplum-db/GreenplumPython",
     packages=setuptools.find_packages(include=["greenplumpython", "greenplumpython.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
