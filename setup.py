9bdbbc86760df8e4a9e238c0ee88521c889761fb
yihong0618 <zouzou0208@gmail.com>
2022-08-30 17:19:48 +0800

    fix: build

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 import setuptools
 
-import greenplumpython
 
 setuptools.setup(
     name="greenplum-python",
-    version=greenplumpython.__version__,
     install_requires=["psycopg2==2.9.3"],
-    author="Greenplum Python",
-    author_email="greenplum-python@vmware.com",
-    description="Python interface for Greenplum and Postgres",
-    long_description="Python interface for Greenplum and Postgres",
-    long_description_content_type="text/markdown",
-    url="https://github.com/greenplum-db/GreenplumPython",
-    packages=setuptools.find_packages(include=["greenplumpython", "greenplumpython.*"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
 )
