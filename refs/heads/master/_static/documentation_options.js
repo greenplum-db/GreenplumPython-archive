f1e17e9aea8298f8fb57ad8dc0b82ed4184d474a
GitHub Action <action@github.com>
2022-09-02 07:23:05 +0000

    Deploy docs from master @ e328f0a

--- refs/heads/master/_static/documentation_options.js
+++ refs/heads/master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Beta 2.0',
+    VERSION: '1.0.0-beta2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
