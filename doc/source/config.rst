5459d8724b908a77ea4a1fdd9ff04beaae0744ad
Ruxue Zeng <zeng_ruxue@126.com>
2022-11-28 15:16:28 +0100

    Format doc

--- doc/source/config.rst
+++ doc/source/config.rst
@@ -2,7 +2,10 @@ Configuration
 =============
 .. module:: greenplumpython
 
+This file contains all the variables to configure GreenplumPython, users can directly set them by assigning value.
+
 .. automodule:: config
    :members:
    :show-inheritance:
    :member-order: bysource
+   :noindex: print_sql
--- greenplumpython/config.py
+++ greenplumpython/config.py
@@ -1,9 +1,6 @@
-"""
-This file contains all the variables to configure GreenplumPython, users can directly set them by assign value to them.
-"""
+# This file contains all the variables to configure GreenplumPython, users can directly set them by assigning value.
 
 print_sql: bool = False
 """
-:noindex:
-This variable configure to display or not the SQL query sent by GreenplumPython to Database behind each command.
+    This variable configure to display or not the SQL query sent by GreenplumPython to Database behind each command.
 """
