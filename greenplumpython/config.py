9070dd8b5f15537c4afec978f6cfc918848acbff
Ruxue Zeng <zeng_ruxue@126.com>
2022-11-28 14:42:56 +0100

    Format doc

--- greenplumpython/config.py
+++ greenplumpython/config.py
@@ -4,5 +4,6 @@ This file contains all the variables to configure GreenplumPython, users can dir
 
 print_sql: bool = False
 """
+:noindex:
 This variable configure to display or not the SQL query sent by GreenplumPython to Database behind each command.
 """
