8c756a39be9d31bd2e3222abcf679860f76e4090
Ruxue Zeng <zeng_ruxue@126.com>
2022-11-29 13:41:33 +0100

    Modify import greenplumpython.config in db.py

--- greenplumpython/db.py
+++ greenplumpython/db.py
@@ -3,7 +3,7 @@ This  module can create a connection to a Greenplum database
 """
 from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, List, Optional, Tuple
 
-import greenplumpython.config
+import greenplumpython.config as gp_config
 
 if TYPE_CHECKING:
     from greenplumpython.table import Table
@@ -45,7 +45,7 @@ class Database:
         """
 
         with self._conn.cursor() as cursor:
-            if greenplumpython.config.print_sql:
+            if gp_config.print_sql:
                 print(query)
             cursor.execute(query)
             return cursor.fetchall() if has_results else None
