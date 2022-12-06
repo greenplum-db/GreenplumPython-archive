b8929cb14c3946645df980061c67857359cdcc42
Ruxue Zeng <36695415+Quasimewdo@users.noreply.github.com>
2022-12-06 10:10:58 +0100

    Fix Join default select empty problem (#122)
    This patch sets the default select columns of join to all. 
    It removes also `Table.rename()` so the API can handle itself 
    self join, the user doesn't need to handle rename Table.

--- tests/test_join.py
+++ tests/test_join.py
@@ -49,19 +49,23 @@ def test_join_all_and_all_columns(db: gp.Database, t1: gp.Table, t2: gp.Table):
             )
         )
     )
-    for col in ["id1", "idd1", "n1", "id2", "idd2", "n2"]:
-        assert col in row
+    assert sorted(row.column_names()) == sorted(["id1", "idd1", "n1", "id2", "idd2", "n2"])
 
 
 def test_join_no_and_no_columns(db: gp.Database, t1: gp.Table, t2: gp.Table):
     ret = next(iter(t1.join(t2, cond=lambda t1, t2: t1["id1"] == t2["id2"])))
-    assert "id1" not in ret and "id2" not in ret  # NOTE: The row is not empty with psycopg2
+    assert sorted(ret.column_names()) == sorted(["id1", "idd1", "n1", "id2", "idd2", "n2"])
 
 
 def test_join_all_and_no_columns(db: gp.Database, t1: gp.Table, t2: gp.Table):
-    ret = next(iter(t1.join(t2, cond=lambda t1, t2: t1["id1"] == t2["id2"], other_columns={"*"})))
-    for col in ["id2", "idd2", "n2"]:
-        assert col in ret
+    ret = next(
+        iter(
+            t1.join(
+                t2, cond=lambda t1, t2: t1["id1"] == t2["id2"], self_columns={}, other_columns={"*"}
+            )
+        )
+    )
+    assert sorted(ret.column_names()) == sorted(["id2", "idd2", "n2"])
 
 
 def test_join_all_and_one_columns(db: gp.Database, t1: gp.Table, t2: gp.Table):
@@ -75,8 +79,7 @@ def test_join_all_and_one_columns(db: gp.Database, t1: gp.Table, t2: gp.Table):
             )
         )
     )
-    for col in ["id2", "idd2", "n2", "id1"]:
-        assert col in ret
+    assert sorted(ret.column_names()) == sorted(["id1", "id2", "idd2", "n2"])
 
 
 def test_join_columns_from_list(db: gp.Database, t1: gp.Table, t2: gp.Table):
@@ -90,8 +93,7 @@ def test_join_columns_from_list(db: gp.Database, t1: gp.Table, t2: gp.Table):
             )
         )
     )
-    for col in ["idd2", "n1", "id1"]:
-        assert col in ret
+    assert sorted(ret.column_names()) == sorted(["id1", "n1", "idd2"])
 
 
 # FIXME : Test for no exist target column
@@ -102,8 +104,7 @@ def test_join_same_column_using(db: gp.Database):
     t1 = gp.to_table(rows, db=db, column_names=["id"])
     t2 = gp.to_table(rows, db=db, column_names=["id"])
     ret = t1.join(t2, using=["id"], self_columns={"id": "t1_id"}, other_columns={"id": "t2_id"})
-    for row in ret:
-        assert "t1_id" in row and "t2_id" in row
+    assert sorted(next(iter(ret)).column_names()) == sorted(["t1_id", "t2_id"])
 
 
 def test_join_same_column_names(db: gp.Database):
@@ -201,8 +202,9 @@ def test_join_natural(db: gp.Database):
         other_columns={"product_name"},
     )
     assert len(list(ret)) == 6
-    for col in ["category_id", "category_name", "product_name"]:
-        assert col in next(iter(ret))
+    assert sorted(next(iter(ret)).column_names()) == sorted(
+        ["category_id", "category_name", "product_name"]
+    )
     for row in ret:
         if row["category_name"] == "Smart Phone":
             assert row["category_id"] == 1
@@ -233,9 +235,8 @@ def test_table_cross_join(db: gp.Database, zoo_1: gp.Table, zoo_2: gp.Table):
 
 
 def test_table_self_join(db: gp.Database, zoo_1: gp.Table):
-    zoo_2 = zoo_1.rename("zoo2")
     ret: gp.Table = zoo_1.join(
-        zoo_2,
+        zoo_1,
         using=["animal"],
         self_columns={"animal": "zoo1_animal", "id": "zoo1_id"},
         other_columns={"animal": "zoo2_animal", "id": "zoo2_id"},
@@ -246,17 +247,22 @@ def test_table_self_join(db: gp.Database, zoo_1: gp.Table):
 
 
 def test_table_join_save(db: gp.Database, zoo_1: gp.Table):
-    zoo_2 = zoo_1.rename("zoo2")
     t_join: gp.Table = zoo_1.join(
-        zoo_2,
+        zoo_1,
         using=["animal"],
         self_columns={"animal": "zoo1_animal", "id": "zoo1_id"},
         other_columns={"animal": "zoo2_animal", "id": "zoo2_id"},
     )
     t_join.save_as("table_join", temp=True)
     t_join_reload = gp.table("table_join", db=db)
-    for col in ["zoo1_id", "zoo1_animal", "zoo2_id", "zoo2_animal"]:
-        assert col in next(iter(t_join_reload)).column_names()
+    assert sorted(next(iter(t_join_reload)).column_names()) == sorted(
+        [
+            "zoo1_animal",
+            "zoo1_id",
+            "zoo2_animal",
+            "zoo2_id",
+        ]
+    )
     for row in t_join_reload:
         assert row["zoo1_animal"] == row["zoo2_animal"]
 
@@ -266,28 +272,22 @@ def test_table_join_ine(db: gp.Database):
     rows2 = [(2,), (3,), (4,)]
     t1 = gp.to_table(rows1, db=db, column_names=["a"])
     t2 = gp.to_table(rows2, db=db, column_names=["b"])
-    ret = t1.join(
-        t2, cond=lambda t1, t2: t1["a"] < t2["b"], self_columns={"a"}, other_columns={"b"}
-    )
+    ret = t1.join(t2, cond=lambda t1, t2: t1["a"] < t2["b"])
     assert len(list(ret)) == 6
     for row in ret:
         assert row["a"] < row["b"]
 
 
 def test_table_multiple_self_join(db: gp.Database, zoo_1: gp.Table):
-    zoo_2 = zoo_1.rename("zoo2")
-    zoo_3 = zoo_2.rename("zoo3")
     t_join = zoo_1.join(
-        zoo_2,
+        zoo_1,
         using=["animal"],
         self_columns={"animal": "zoo1_animal", "id": "zoo1_id"},
         other_columns={"animal": "zoo2_animal", "id": "zoo2_id"},
     )
     ret = t_join.join(
-        zoo_3,
+        zoo_1,
         cond=lambda s, o: s["zoo1_animal"] == o["animal"],
-        self_columns={"*"},
-        other_columns={"*"},
     )
     assert len(list(ret)) == 4
     for row in ret:
@@ -304,5 +304,5 @@ def test_lineage_dfs_order(db: gp.Database):
     numbers = gp.to_table(rows, db=db, column_names=["val"])
     mod = numbers.assign(mod=lambda t: t["val"] % 2)
     mod3 = mod.assign(mod3=lambda t: t["val"] % 3)
-    results: gp.Table = mod3.join(numbers, using=["val"], self_columns={"val"})
+    results: gp.Table = mod3.join(numbers, using=["val"], other_columns={})
     assert len(list(results)) == 10
