8d905f450a0ae5bbba85080412b007885e6d488f
Ruxue Zeng <36695415+Quasimewdo@users.noreply.github.com>
2023-01-12 10:44:58 +0100

    Modification format of __repr__ (#133)
    Make the output of __repr__ similar to Greenplum

--- greenplumpython/table.py
+++ greenplumpython/table.py
@@ -180,31 +180,38 @@ class Table:
         repr_string: str = ""
         if len(list(self)) != 0:
             # Iterate over the given table to calculate the column width for its ASCII representation.
-            width = [0] * len(next(iter(self)).column_names())
+            col_number = len(next(iter(self)).column_names())
+            width = [0] * col_number
             for row in self:
                 for col_idx, col in enumerate(row):
                     width[col_idx] = max(width[col_idx], len(col), len(str(row[col])))
-
             # Table header.
             repr_string += (
                 "".join(
                     [
-                        "| {:{}} |".format(col, width[idx])
+                        " {:{}} ".format(col, width[idx]) + ("|" if idx < col_number - 1 else "")
                         for idx, col in enumerate(next(iter(self)))
                     ]
                 )
                 + "\n"
             )
             # Dividing line below table header.
-            repr_string += ("=" * (sum(width) + 4 * len(width))) + "\n"
+            repr_string += (
+                "+".join(
+                    [" {:{}} ".format("-" * width[idx], width[idx]) for idx in range(col_number)]
+                )
+                + "\n"
+            )
             # Table contents.
             for row in self:
                 content = [row[c] for c in row]
                 for idx, c in enumerate(content):
                     if isinstance(c, list):
-                        repr_string += ("| {:{}} |").format("{}".format(c), width[idx])  # type: ignore
+                        repr_string += (" {:{}} ").format("{}".format(c), width[idx])  # type: ignore
+                        repr_string += "|" if idx < col_number - 1 else ""
                     else:
-                        repr_string += ("| {:{}} |").format(c if c is not None else "", width[idx])
+                        repr_string += (" {:{}} ").format(c if c is not None else "", width[idx])
+                        repr_string += "|" if idx < col_number - 1 else ""
                 repr_string += "\n"
         return repr_string
 
