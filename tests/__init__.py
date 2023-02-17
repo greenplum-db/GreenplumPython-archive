b8d324687c31fc14b75db1f4c6566333e84e6290
Xuebin Su <sxuebin@vmware.com>
2023-02-16 19:41:14 -0500

    Fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,10 +9,12 @@ import greenplumpython as gp
 def db():
     # for the connection both work for GitHub Actions and concourse
     db = gp.database(
-        host="localhost",
-        dbname=environ.get("TESTDB", "gpadmin"),
-        user=environ.get("PGUSER"),
-        password=environ.get("PGPASSWORD"),
+        params={
+            "host": "localhost",
+            "dbname": environ.get("TESTDB", "gpadmin"),
+            "user": environ.get("PGUSER"),
+            "password": environ.get("PGPASSWORD"),
+        }
     )
     yield db
     db.close()
