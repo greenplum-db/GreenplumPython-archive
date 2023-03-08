047263d0bf6f0902f5d2b505086b93fed3d33281
Ruxue Zeng <zeng_ruxue@126.com>
2023-03-08 09:54:01 +0100

    - Make test_join_self_cond simpler - Make join() code cleaner

--- greenplumpython/dataframe.py
+++ greenplumpython/dataframe.py
@@ -623,19 +623,12 @@ class DataFrame:
         other_clause = (
             other._name if self._name != other._name else self._name + " AS " + other_name
         )
+        other_temp = other if self._name != other._name else DataFrame(query="", name=other_name)
         target_list = bind(self, self_columns) + bind(
             DataFrame(query="", name=other_name), other_columns
         )
         # ON clause in SQL uses argument `cond`.
-        sql_on_clause = (
-            (
-                f"ON {cond(self, other)._serialize()}"
-                if self._name != other._name
-                else f"ON {cond(self, DataFrame(query='', name=other_name))._serialize()}"
-            )
-            if cond is not None
-            else ""
-        )
+        sql_on_clause = f"ON {cond(self, other_temp)._serialize()}" if cond is not None else ""
         join_column_names = (
             (f'"{on}"' if isinstance(on, str) else ",".join([f'"{name}"' for name in on]))
             if on is not None
