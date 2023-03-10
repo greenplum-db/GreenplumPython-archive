ee59fca4842994bc1978465a9e17eb92fc9309eb
Ruxue Zeng <zeng_ruxue@126.com>
2023-03-10 10:05:05 +0100

    Clean test_type.py

--- tests/test_type.py
+++ tests/test_type.py
@@ -7,16 +7,6 @@ from greenplumpython.type import to_pg_type
 from tests import db
 
 
-# Since we set all table_name to cte_hex(), we can't use regclass(t["tableoid"]) anymore
-# def test_type_cast(db: gp.Database):
-#     rows = [(i,) for i in range(10)]
-#     series = db.create_dataframe(rows=rows, column_names=["val"]).save_as(
-#         "series", column_names=["val"], temp=True
-#     )
-#     regclass = gp.type_("regclass")
-#     dataframe_name = series.assign(dataframe_name=lambda t: regclass(t["tableoid"]))
-#     for row in dataframe_name:
-#         assert row["dataframe_name"] == "series"
 def test_type_cast(db: gp.Database):
     db._execute(
         f"""
