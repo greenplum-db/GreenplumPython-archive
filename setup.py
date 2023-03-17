2c3412b012180874c90c5670e21f441f9cd2b2f9
Xuebin Su (苏学斌) <12034000+xuebinsu@users.noreply.github.com>
2023-03-17 16:20:01 +0800

    Fix display issue of README on PyPI (#188)
    Previsouly, PyPI displayed the README as plain text. This is because it
    does not know the content type of the file.
    
    This patch fixes the issue by adding a argument in setup() to tell PyPI
    to render the README as Markdown.
    
    Reference: 
    https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+# read the contents of your README file
+from pathlib import Path
+
 import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="greenplum-python",
     install_requires=["psycopg2-binary==2.9.5", "dill==0.3.6"],
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
