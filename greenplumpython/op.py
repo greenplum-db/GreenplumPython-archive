67144202baea128d622d66c9493fc9f84bb4a2ef
Xuebin Su <sxuebin@vmware.com>
2023-06-21 06:57:44 -0400

    Hide op's qualified_name

--- greenplumpython/op.py
+++ greenplumpython/op.py
@@ -59,7 +59,7 @@ class Operator:
         self._schema = schema
 
     @property
-    def qualified_name(self) -> str:
+    def _qualified_name(self) -> str:
         if self._schema is not None:
             return f'OPERATOR("{self._schema}".{self._name})'
         else:
@@ -67,9 +67,9 @@ class Operator:
 
     def __call__(self, *args: Any) -> Union[UnaryExpr, BinaryExpr]:
         if len(args) == 1:
-            return UnaryExpr(self.qualified_name, args[0])
+            return UnaryExpr(self._qualified_name, args[0])
         if len(args) == 2:
-            return BinaryExpr(self.qualified_name, args[0], args[1])
+            return BinaryExpr(self._qualified_name, args[0], args[1])
         else:
             raise Exception("Too many operands.")
 
