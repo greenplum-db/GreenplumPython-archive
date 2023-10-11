3655229fc812ad047ca7c3fa541f45d5cc9bb258
Xuebin Su <sxuebin@vmware.com>
2023-10-10 23:21:52 -0400

    Install PL/Python3

--- concourse/test.sh
+++ concourse/test.sh
@@ -2,7 +2,20 @@
 
 set -exo pipefail
 
+function install_plpython3() {
+    if [[ $GP_MAJOR_VERSION == 6 ]]; then
+        mkdir -p bin_plpython3/install_tmp
+        pushd bin_plpython3/install_tmp
+        find .. -maxdepth 1 -regex ".*-[0-9\.]*-.*\.tar\.gz" -exec tar xfv {} \;
+        ./install_gpdb_component
+        popd
+    fi
+}
+
 function _main() {
+    start_gpdb
+    install_plpython3
+
     # FIXME: The test db and extension creation should be handled by python code.
     createdb gpadmin
     psql gpadmin -c "CREATE LANGUAGE plpython3u;"
