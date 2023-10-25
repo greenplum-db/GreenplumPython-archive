09f81a77cb3435dab403f757810cc5eba5d98413
Xuebin Su <sxuebin@vmware.com>
2023-10-25 03:57:23 -0400

    Add alias for su

--- concourse/test.sh
+++ concourse/test.sh
@@ -16,7 +16,7 @@ function _main() {
     install_plpython3
 
     source "$CI_REPO_DIR/common/entry_common.sh"
-    sudo passwd --delete gpadmin  # for `su gpadmin` in start_gpdb
+    alias su='sudo su'  # for `su gpadmin` in start_gpdb
     start_gpdb
     source "$HOME/.bashrc"  # for gpdemo-env.sh
 
