cffc058dd53c52ade4f8be87486867f14fcbc11f
Xuebin Su <sxuebin@vmware.com>
2023-10-25 23:22:56 -0400

    Make test.sh idempotent

--- concourse/test.sh
+++ concourse/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
-set -exo pipefail
+set -o xtrace -o errexit -o nounset -o pipefail
 
-function install_plpython3() {
-    if [[ $GP_MAJOR_VERSION == 6 ]]; then
+install_plpython3() {
+    if [[ $GP_MAJOR_VERSION == 6 && ! -f "$(pg_config --pkglibdir)/plpython3.so" ]]; then
         mkdir -p bin_plpython3/install_tmp
         pushd bin_plpython3/install_tmp
         find .. -maxdepth 1 -regex ".*-[0-9\.]*-.*\.tar\.gz" -exec tar xfv {} \;
@@ -12,20 +12,33 @@ function install_plpython3() {
     fi
 }
 
-function _main() {
-    install_plpython3
+start_gpdb_as_gpadmin() {
+    if ! gpstate; then
+        source "$CI_REPO_DIR/common/entry_common.sh"
+        sudo passwd --delete gpadmin  # for `su gpadmin` in start_gpdb
+        if [[ "$(source /etc/os-release && echo $ID)" == "ubuntu" ]]; then
+            su () {
+                sudo "su" "$@"
+            }
+        fi
+        start_gpdb
+        source "$HOME/.bashrc"  # for gpdemo-env.sh
+    fi
+}
 
-    source "$CI_REPO_DIR/common/entry_common.sh"
-    sudo passwd --delete gpadmin  # for `su gpadmin` in start_gpdb
-    su () {
-        sudo "su" "$@"
-    }
-    start_gpdb
-    source "$HOME/.bashrc"  # for gpdemo-env.sh
-
+setup_testdb() {
+    local testdb=$1
     # FIXME: The test db and extension creation should be handled by python code.
-    createdb gpadmin
-    psql gpadmin -c "CREATE LANGUAGE plpython3u;"
+    if psql "$testdb" -c ''; then
+        createdb "$testdb"
+    fi
+    psql "$testdb" -c "CREATE EXTENSION IF NOT EXISTS plpython3u;"
+}
+
+_main() {
+    install_plpython3
+    start_gpdb_as_gpadmin
+    setup_testdb gpadmin
 
     # Run testing
     pushd /home/gpadmin/greenplumpython_src
