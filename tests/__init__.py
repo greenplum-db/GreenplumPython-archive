1333f38d24a7f5bdaca78597b0186f4b53a72e74
Xuebin Su <sxuebin@vmware.com>
2023-11-09 06:30:12 -0500

    Print debug message

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,6 +7,7 @@ import greenplumpython as gp
 
 @pytest.fixture()
 def db():
+    print("connecting ...")
     # for the connection both work for GitHub Actions and concourse
     db = gp.database(
         params={
@@ -20,15 +21,15 @@ def db():
         """
         CREATE EXTENSION IF NOT EXISTS plpython3u;
         CREATE EXTENSION IF NOT EXISTS vector;
-        """, 
-        has_results=False
+        """,
+        has_results=False,
     )
     db._execute(
         """
         DROP SCHEMA IF EXISTS test CASCADE;
         CREATE SCHEMA test;
-        """, 
-        has_results=False
+        """,
+        has_results=False,
     )
     yield db
     db.close()
