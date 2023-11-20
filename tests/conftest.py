9e082e6a7be584d241b4d1a4a3403ae73f220688
Xuebin Su <sxuebin@vmware.com>
2023-11-19 20:46:29 -0500

    Make pgvector optional

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,21 @@ def pytest_addoption(parser: pytest.Parser):
         default=True,
         help="Use pickler to deserialize UDFs on server.",
     )
+    parser.addini(
+        "server_has_pgvector",
+        type="bool",
+        default=True,
+        help="pgvector is available on server.",
+    )
 
 
 @pytest.fixture(scope="session")
 def server_use_pickler(pytestconfig: pytest.Config) -> bool:
     val: bool = pytestconfig.getini("server_use_pickler")
+    return val
+
+
+@pytest.fixture(scope="session")
+def server_has_pgvector(pytestconfig: pytest.Config) -> bool:
+    val: bool = pytestconfig.getini("server_has_pgvector")
     return val
