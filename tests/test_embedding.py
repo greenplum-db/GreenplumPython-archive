2147a5958453a947b481c25baf2534e1abb7bb81
Xuebin Su (苏学斌) <12034000+xuebinsu@users.noreply.github.com>
2023-12-07 10:38:22 +0800

    Fix missing unique key when searching embeddings (#226)
    Unique key is required when searching embeddings to join the
    embedding table and the original data table before returning the
    results. Previously, search on a dataframe that created from an
    existing table in database failed due to lacking of unique key in the
    dataframe.
    
    This patch fixes the issue by recording the unique key when
    `create_index()` in `pg_class` so that the info can be read when
    `search()`.

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -1,15 +1,24 @@
 import pytest
 
 import greenplumpython as gp
+import greenplumpython.experimental.embedding as _
 from tests import db
 
 
+def search_embeddings(t: gp.DataFrame):
+    results = t.embedding().search(column="content", query="apple", top_k=1)
+    assert len(list(results)) == 1
+    row = next(iter(results))
+    assert row["content"] == "I like eating apples."
+
+
 @pytest.mark.requires_pgvector
-def test_embedding_query_string(db: gp.Database):
+def test_embedding_query_text(db: gp.Database):
     content = ["I have a dog.", "I like eating apples."]
     t = (
         db.create_dataframe(columns={"id": range(len(content)), "content": content})
         .save_as(
+            table_name="doc",
             temp=True,
             column_names=["id", "content"],
             distribution_key={"id"},
@@ -20,7 +29,34 @@ def test_embedding_query_string(db: gp.Database):
         .check_unique(columns={"id"})
     )
     t = t.embedding().create_index(column="content", model_name="all-MiniLM-L6-v2")
-    df = t.embedding().search(column="content", query="apple", top_k=1)
-    assert len(list(df)) == 1
-    for row in df:
-        assert row["content"] == "I like eating apples."
+    search_embeddings(t)
+
+    # Ensure that a new DataFrame created from table in database can also be
+    # searched.
+    search_embeddings(db.create_dataframe("doc", schema="pg_temp"))
+
+
+@pytest.mark.requires_pgvector
+def test_embedding_multi_col_unique(db: gp.Database):
+    content = ["I have a dog.", "I like eating apples."]
+    columns = {"id": range(len(content)), "id2": [1] * len(content), "content": content}
+    t = (
+        db.create_dataframe(columns=columns)
+        .save_as(
+            temp=True,
+            column_names=list(columns.keys()),
+            distribution_key={"id"},
+            distribution_type="hash",
+            drop_if_exists=True,
+            drop_cascade=True,
+        )
+        .check_unique(columns={"id", "id2"})
+    )
+    t = t.embedding().create_index(column="content", model_name="all-MiniLM-L6-v2")
+    print(
+        "reloptions =",
+        db._execute(
+            f"SELECT reloptions FROM pg_class WHERE oid = '{t._qualified_table_name}'::regclass"
+        ),
+    )
+    search_embeddings(t)
